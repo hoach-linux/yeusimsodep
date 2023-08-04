057a09a996fec2ca91b5bd07de1bb25a6a8065a5
hoach-linux <hoachprogrammer@gmail.com>
2023-08-04 15:07:53 +0700

    added delete by search and redesign

--- src/hooks/useCheckingRegister.ts
+++ src/hooks/useCheckingRegister.ts
@@ -5,12 +5,12 @@ export const useCheckingRegister = (link: string) => {
   const registered = [localStorage.getItem("sb-wzlfkkdcqditzrbxeioy-auth-token"), localStorage.getItem("directus_token")];
 
   const checkRegister = () => {
-    if (registered[0] && registered[1]) return true;
+    if (registered[0] || registered[1]) return true;
 
     return navigate(link);
   };
   const checkNotRegister = () => {
-    if (!registered[0] && !registered[1]) return true;
+    if (!registered[0] || !registered[1]) return true;
 
     return navigate(link);
   };
