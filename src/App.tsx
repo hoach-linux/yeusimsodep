b45e577c7aec09bb8fef177b6aa3e94f6cf1fff7
hoach-linux <hoachprogrammer@gmail.com>
2023-08-05 23:26:33 +0700

    added scroll to top button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,28 @@ import "./style/main.css";
 import Navbar from "./components/Navbar";
 import { Box } from "./components/Box";
 import { Outlet } from "react-router-dom";
-import { CssBaseline, ThemeProvider, responsiveFontSizes } from "@mui/material";
+import { CssBaseline, Fab, ThemeProvider, Toolbar, responsiveFontSizes } from "@mui/material";
 import useStore from "./store/useStore";
 import { MuiThemeCustom } from "./plugin/MuiThemeCustom";
+import ScrollTop from "./components/scrollToTop";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useTheme, Theme } from "@mui/material/styles";
 
-function App() {
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+function App(props: Props) {
   const mode = useStore((state: any) => state.mode)
   const lightMode = useStore((state: any) => state.lightMode)
+  const theme: Theme = useTheme()
+  const isDarkMode = theme.palette.mode === 'dark'
 
   if (mode === null) {
     window.localStorage.setItem("data-theme", "light")
@@ -16,13 +31,14 @@ function App() {
     lightMode
   }
 
-  let theme = MuiThemeCustom({ mode })
-  theme = responsiveFontSizes(theme)
+  let customTheme = MuiThemeCustom({ mode })
+  customTheme = responsiveFontSizes(customTheme)
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={customTheme}>
       <CssBaseline />
       <Navbar />
+      <Toolbar id="back-to-top-anchor" />
       <Box
         css={{
           maxWidth: "1380px",
@@ -32,6 +48,18 @@ function App() {
       >
         <Outlet />
       </Box>
+      <ScrollTop {...props}>
+        <Fab aria-label="scroll back to top"
+          sx={{
+            border: isDarkMode ? "1px solid #313131" : "1px solid #ECECEC",
+            background: isDarkMode ? "rgba(31, 31, 31, 0.5)" : "rgba(245, 245, 246, 0.5)",
+            backdropFilter: "blur(8px)",
+            boxShadow: "none"
+          }}
+        >
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
     </ThemeProvider>
   );
 }
